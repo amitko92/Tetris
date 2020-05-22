dcd5449b4ec8c80aff018dba8023d19226132bb9
Amit Koren <30484152+amitko92@users.noreply.github.com>
2020-05-22 10:56:20 +0300

    finished on html and css

--- appRe/script.js
+++ appRe/script.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
     const grid = document.querySelector('.tetris-grid')
-
+    let squares = Array.from(document.querySelectorAll('.tetris-grid grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const levelDisplay = document.querySelector('#level')
+    const startBtn = document.querySelector('#start-button')
+    const restartBtn = document.querySelector('#restart-button')
+    const width = 10
+    let nextRandom = 0
+    let timerId
+    let score = 0
+    let level = 0
 
 
 
