a178d6c7445e84f48e0ba29f9eb04cb8020d945b
Amit Koren <30484152+amitko92@users.noreply.github.com>
2020-05-28 12:40:01 +0300

    update js and html

--- static/js/script.js
+++ static/js/script.js
@@ -8,9 +8,27 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const startBtn = document.querySelector('#start-button')
     const restartBtn = document.querySelector('#restart-button')
     const scoreName1 = document.querySelector('#name-1')
+
+    const tableDate1 = document.querySelector('#date-1')
+    const tableLevel1 = document.querySelector('#level-1')
+    const tableScore1 = document.querySelector('#score-1')
+
+    const tableDate2 = document.querySelector('#date-2')
+    const tableLevel2 = document.querySelector('#level-2')
+    const tableScore2 = document.querySelector('#score-2')
+
+    const tableDate3 = document.querySelector('#date-3')
+    const tableLevel3 = document.querySelector('#level-3')
+    const tableScore3 = document.querySelector('#score-3')
+
+    const score1 = [tableDate1, tableLevel1, tableScore1]
+    const score2 = [tableDate2, tableLevel2, tableScore2]
+    const score3 = [tableDate3, tableLevel3, tableScore3]
+    tableScore = [score1, score2, score3]
+
     const width = 10
     displayWidth = 4
-    let startGridColor =  '#59ec1f'
+    let gridColor =  '#59ec1f'
     let nextRandom = 0
     let timerId
     let score = 0
@@ -20,13 +38,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let currentSpeed = 1000
     const speedReruction = 150
 
-    const names = [
-      'amit',
-      'roi',
-      'noam',
-      ''
-    ]
-
     const colors = [
         'orange',
         'red',
@@ -35,12 +46,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
         'blue'
       ]
 
-    const backgroundColor = [
-      '#00a000',
-      '#ffe100',
-      '#ffa800',
-      '#ff0000'
-    ]
+  updateLocalStorage()
+  updateBoardScore()
 
     //The Tetrominoes
   const lTetromino = [
@@ -87,6 +94,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
   let random = Math.floor(Math.random()*theTetrominoes.length)
   let current = theTetrominoes[random][currentRotation]
 
+  startBtn.addEventListener('click', ()=>{
+    startOrPauseFunc()
+  })
+
+  restartBtn.addEventListener('click', ()=>{
+    startOrPauseFunc()
+    currentRotation = 0
+    random = Math.floor(Math.random() * theTetrominoes.length)
+    current = theTetrominoes[random][currentRotation]
+    restartFunc()
+  })
+
+  document.addEventListener('keyup', control)
+
   //draw the Tetromino
   function draw() {
     current.forEach(index => {
@@ -119,8 +140,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       moveDown()
     }
   }  
-  document.addEventListener('keyup', control)
-
+  
   //move down function
   function moveDown() {
     undraw()
@@ -151,10 +171,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
     })
   }
-
   
-
-  //freeze function
   function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
@@ -216,7 +233,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     for(i = 0; i < squares.length - width; i++)
     {
-        squares[i].style.backgroundColor = startGridColor;
+        squares[i].style.backgroundColor = gridColor;
         squares[i].classList.remove('taken')
         squares[i].classList.remove('tetromino')
     }
@@ -234,56 +251,40 @@ document.addEventListener('DOMContentLoaded', ()=>{
       }
   }
 
-    //add score
-    function addScore() {
-        let counter = 0
-        for (let i = 0; i < 199; i +=width) {
-          const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
-    
-          if(row.every(index => squares[index].classList.contains('taken'))) {
-            counter += 1
-            row.forEach(index => {
-              squares[index].classList.remove('taken')
-              squares[index].classList.remove('tetromino')
-              squares[index].style.backgroundColor = ''
-            })
-            const squaresRemoved = squares.splice(i, width)
-            squares = squaresRemoved.concat(squares)
-            squares.forEach(cell => grid.appendChild(cell))
-          }
-        }
-        if(counter <= 1){
-            console.log("the current score is, " + score + ", and add " +  counter*10 + counter*5)
-            score += counter*10
-        }
-        else{
-            console.log("the current score is, " + score + ", and add " +  counter*10 + counter*5)
-            score += counter*10 + counter*1
-        }
-        scoreDisplay.innerHTML = score
+  //add score
+  function addScore() {
+    let counter = 0
+    for (let i = 0; i < 199; i +=width) {
+      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
 
+      if(row.every(index => squares[index].classList.contains('taken'))) {
+        counter += 1
+        row.forEach(index => {
+          squares[index].classList.remove('taken')
+          squares[index].classList.remove('tetromino')
+          squares[index].style.backgroundColor = ''
+        })
+        const squaresRemoved = squares.splice(i, width)
+        squares = squaresRemoved.concat(squares)
+        squares.forEach(cell => grid.appendChild(cell))
       }
+    }
+    if(counter <= 1)
+        score += counter*10
+    else
+        score += counter*10 + counter*1
+    scoreDisplay.innerHTML = score
+    updateLocalStorage()
+  }
     
-      //game over
-      function gameOver() {
-        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-          window.alert("Game Over, your score is: " + score)
-          clearInterval(timerId)
-          restartFunc();
-        }
-      }
-
-  startBtn.addEventListener('click', ()=>{
-    startOrPauseFunc()
-  })
-
-  restartBtn.addEventListener('click', ()=>{
-    startOrPauseFunc()
-    currentRotation = 0
-    random = Math.floor(Math.random() * theTetrominoes.length)
-    current = theTetrominoes[random][currentRotation]
-    restartFunc()
-  })
+  //game over
+  function gameOver() {
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      window.alert("Game Over, your score is: " + score)
+      clearInterval(timerId)
+      restartFunc();
+    }
+  }
 
   function levelUp(){
     counterLevel += 1
@@ -303,6 +304,42 @@ document.addEventListener('DOMContentLoaded', ()=>{
       currentSpeed -= speedReruction
       timerId = setInterval(moveDown, currentSpeed)
     }
+  }
+
+  function updateLocalStorage(){
+    if(localStorage.length != 2){
+      for(i = localStorage.length; i < 3; i++){
+        localStorage.setItem(i,"dd/mm/yy 0 0")
+      }
+    }
+
+    for(i = 0; i < 3; i++){
+      if(score >= localStorage.getItem(i).split(' ')[2]){
+        
+        for(j = i; j < 3; j++){
+          localStorage.setItem(j + 1, localStorage.getItem(j))
+        }
+
+        str = getDate() + " "+ level + " " + score
+        localStorage.setItem(i, str)
+        break
+      }
+    }
+    updateBoardScore()
+  }
+
+  function updateBoardScore(){
+    
+    for(i = 0; i < tableScore.length; i++){
+      arr = localStorage.getItem(i).split(' ')
+      for(j = 0; j < tableScore[i].length; j++){
+        tableScore[i][j].innerHTML = arr[j]
+      }
+    }
+  }
+
+  function getDate(){
+    return "15/03/20"
   }
 
 })
